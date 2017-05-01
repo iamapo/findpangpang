6028c32356c7ea2e2e2c2d1ba6d3acfff9ddc97f
iamapo <iamapo@Andres-MBP.fritz.box>
2017-05-01 21:06:32 +0200

    Set Up

---
+++ FindPangPang/app/src/main/java/com/bamandpang/findpangpang/LoginActivity.java
@@ -0,0 +1,94 @@
+package com.bamandpang.findpangpang;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private AccessToken accessToken;
+    private AccessTokenTracker accessTokenTracker;
+
+    private static final String MAP_ACTIVITIES = "com.bamandpang.findpangpang.MapsActivity";
+    // Request Code
+    private static final int MAP_ACTIVITIES_REQUEST_CODE = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
+
+        loginButton = (LoginButton) this.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        // Other app specific specialization
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("LOGIN_SUCCESS", "Success");
+                loginButton.setVisibility(View.INVISIBLE); //<- IMPORTANT
+                Intent intent = new Intent(MAP_ACTIVITIES);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+                // Set the access token using
+                // currentAccessToken when it's loaded or set.
+            }
+        };
+        // If the access token is available already assign it.
+        accessToken = AccessToken.getCurrentAccessToken();
+        // If already logged in show the home view
+        if (accessToken != null) {//<- IMPORTANT
+            Intent intent = new Intent(MAP_ACTIVITIES);
+            startActivity(intent);
+            finish();//<- IMPORTANT
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
